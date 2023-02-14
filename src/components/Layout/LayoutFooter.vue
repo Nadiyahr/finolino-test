87a04d85ec95746ff4bad797b658e8810fce3f63
nadiyaHrabchenko <n.hrabchenko@artlabseurope.com>
2023-02-15 01:02:23 +0200

    fix footer grid mobile

--- src/components/Layout/LayoutFooter.vue
+++ src/components/Layout/LayoutFooter.vue
@@ -1,24 +1,26 @@
 <template> 
   <footer class="c-footer">
     <div class="grid grid--desktop u-mb-center">
-      <div class="grid__item grid__item--tablet-1-12 grid__item--desktop-1-5 u-mb-center">
+      <div class="grid__item grid__item--1-2 grid__item--tablet-1-12 grid__item--desktop-1-5 u-mb-center">
         <Logo class="c-footer__logo" />
         <IconButtonGroup v-if="!isMobile" :icons="icons" />
         <p class="u-fw-light u-py-2 u-color-info u-fs-xs">&copy; {{ year }}. All rights reserved</p>
       </div>
-      <div class="grid__item grid__item--tablet-1-4 grid__item--desktop-6-7">
+      <div class="grid__item grid__item--1-2 grid__item--tablet-1-4 grid__item--desktop-6-7">
         <h6 v-if="isMobile">About us</h6>
         <FooterList v-else title="About" :list="about" />
       </div>
-      <div class="grid__item grid__item--tablet-4-8 grid__item--desktop-8-9">
+      <div class="grid__item grid__item--1-2 grid__item--tablet-4-8 grid__item--desktop-8-9">
         <h6 v-if="isMobile" >Privacy policy</h6>
          <FooterList v-else title="Help" :list="help" />
       </div>
-      <div class="grid__item grid__item--tablet-8-12 grid__item--desktop-10-12">
+      <div class="grid__item grid__item--1-2 grid__item--tablet-8-12 grid__item--desktop-10-12">
         <h6 v-if="isMobile"  class="u-pb-5">FAQs</h6>
         <FooterList v-else title="Contacts" :list="contacts" />
       </div>
+      <div class="grid__item grid__item--1-2" >
         <IconButtonGroup v-if="isMobile" :icons="icons" class="u-m-auto" />
+      </div>
     </div>
   </footer>
 </template>
